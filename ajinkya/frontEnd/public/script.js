cc1a0608f5f40ae36bc931fbb4d70755f2afa8b3
Ajinkya Deshpande <ajinkyad1802@gmail.com>
2018-03-27 19:29:17 +0530

    deleting extras directory renaming

--- ajinkya/frontEnd/public/script.js
+++
@@ -1,8 +0,0 @@
-var button = document.querySelector("#theme");
-button.addEventListener("click", function(){
-	document.body.classList.toggle("black");
-});
-var jumbo = document.querySelector(".jumbotron");
-jumbo.addEventListener("click", function(){
-	document.body.classList.toggle("header");
-});
